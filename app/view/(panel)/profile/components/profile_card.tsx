2b70baf8b394bdaf164109b6d59c4501d0e26af8
John Ivan Belaro <belaroivan17@gmail.com>
2025-12-25 16:45:55 +0800

    hello yahooors

--- app/view/(panel)/profile/components/profile_card.tsx
+++ app/view/(panel)/profile/components/profile_card.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Plus, Calendar } from "lucide-react";
+import MaleProfile from "../../../svgs/male_profile";
 
 export default function ProfileCard() {
   return (
@@ -13,15 +14,9 @@ export default function ProfileCard() {
       <div className="px-6 pb-6 relative">
         <div className="flex flex-col lg:flex-row items-center lg:items-start">
           <div className="-mt-12 lg:-mt-16 flex-shrink-0 relative z-10">
-            <div className="w-24 h-24 lg:w-32 lg:h-32 rounded-full border-4 border-white bg-blue-100 flex items-center justify-center overflow-hidden">
-                <span className="text-3xl font-bold text-blue-500">JB</span>
+            <div className="w-24 h-24 lg:w-32 lg:h-32 rounded-full border-4 border-white bg-light_coffee flex items-center justify-center overflow-hidden">
+                <span className="text-3xl font-bold text-light_brown">JB</span>
             </div>
-            
-            <button className="absolute bottom-1 right-1 bg-orange-400 p-1.5 rounded-full text-white border-2 border-white">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-              </svg>
-            </button>
           </div>
           <div className="mt-4 lg:mt-4 lg:ml-6 flex-grow text-center lg:text-left">
             <h1 className="text-2xl font-semibold text-gray-800">
@@ -35,13 +30,15 @@ export default function ProfileCard() {
 
               </div>
               <div className="flex items-center gap-1">
-                <svg className="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" /></svg>
-                <span>4th Year | BSIT</span>
+                <MaleProfile />
+                <span>He/Him</span>
               </div>
 
               <div className="flex items-center gap-1">
-                <svg className="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
-                <span>Purok 1, Cm. Recto, St. San Julian Irosin Sorsogon 4707</span>
+                <div className="text-text_light">
+                  <Calendar size={12} />
+                </div>
+                <span>Created March 17, 2025</span>
               </div>
 
             </div>
