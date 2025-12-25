5f3e02c98a189afd491e0485f2797a9b725e7e97
John Ivan Belaro <belaroivan17@gmail.com>
2025-12-25 23:54:38 +0800

    added functionality for update profile modal

--- app/view/(panel)/profile/components/profile_card.tsx
+++ app/view/(panel)/profile/components/profile_card.tsx
@@ -2,6 +2,7 @@
 
 import { Plus, Calendar } from "lucide-react";
 import MaleProfile from "../../../svgs/male_profile";
+import UpdateProfileModal from "../modals/update_details";
 
 export default function ProfileCard() {
   return (
@@ -44,11 +45,7 @@ export default function ProfileCard() {
             </div>
           </div>
           <div className="mt-4 lg:mt-6 lg:ml-auto">
-             <button className="border border-blue-400 text-blue-500 hover:bg-blue-50 p-2 rounded-lg transition">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                </svg>
-             </button>
+             <UpdateProfileModal />
           </div>
 
         </div>
